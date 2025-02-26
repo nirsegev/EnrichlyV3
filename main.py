1c8dd06e7fda5dae9bf7639f3cdfb3aeb969b1b0
Nir Segev <nirsegev@gmail.com>
2025-02-26 13:58:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
+# Mount the static directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    with open(os.path.join("static", "index.html")) as f:
+        return HTMLResponse(content=f.read(), status_code=200)
