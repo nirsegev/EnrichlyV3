4101e52a609291ca98619b529b7b5545b70a4b52
Nir Segev <nirsegev@gmail.com>
2025-02-26 13:49:00 +0200

    Hello world
    add hello world react based

---
+++ static/main.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [data, setData] = useState({ greeting: '', message: '' });
+
+    useEffect(() => {
+        fetch('/')
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>{data.greeting}</h1>
+            <p>{data.message}</p>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
