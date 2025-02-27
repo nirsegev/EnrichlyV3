c287152652f80ede689ebc12d71712310bea8eb0
Nir Segev <nirsegev@gmail.com>
2025-02-27 05:32:55 +0200

    Basic Hello world

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
-    with open(os.path.join("static", "index.html")) as f:
+    with open(os.path.join("static", "index.html")) as f: 
         return HTMLResponse(content=f.read(), status_code=200)
---
+++ static/app.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+); 
