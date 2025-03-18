194a3ae856bf04a18c3663c885a647e0d4bfb5fd
Nir Segev <nirsegev@gmail.com>
2025-03-18 06:10:00 +0200

    Revert "Fix management service deployement"
    This reverts commit 10f180edb256f7c56041e91bfa1e5fc7e6083078.

--- models/base.py
+++ models/base.py
@@ -2,19 +2,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import get_settings
-import re
 
 settings = get_settings()
 
-# Handle PostgreSQL special URL format from Railway.app
-def get_database_url():
-    url = settings.DATABASE_URL
-    if url.startswith("postgres://"):
-        url = url.replace("postgres://", "postgresql://", 1)
-    return url
-
 # Create database engine
-engine = create_engine(get_database_url())
+engine = create_engine(settings.DATABASE_URL)
 
 # Create declarative base
 Base = declarative_base()
